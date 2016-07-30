286177d33d29e765cea4b8d6eb94605bb080c15d
MMcaravaggio <orcunbahadir@yahoo.com>
2016-07-30 21:09:14 +1000

    Logo

--- src/xhp/Fbbranding.php
+++ src/xhp/Fbbranding.php
@@ -8,8 +8,8 @@ class :fbbranding extends :x:element {
   protected function render(): XHPRoot {
     return
       <span class="branding-el">
-        <svg class="icon icon--social-facebook">
-          <use href="#icon--social-facebook" />
+        <svg class="icon icon--arrow-up">
+          <use href="#icon--arrow-up" />
         </svg>
         <span class="has-icon"> {tr('  Monash University')}</span>
       </span>;
